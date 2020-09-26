71e54b8ac806151d590302e71cc97b895b62d45f
Alexander105 <sadorsadoff@gmail.com>
2020-09-26 12:02:37 +0300

    RouterLesson1

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
-require_once __DIR__ . '/../' . 'vendor/autoload.php';
+include 'global.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Core\Router;
 
 $router = new Router();
-$router->run($router->getVar());
+$router->runShow();
+$router->run();
