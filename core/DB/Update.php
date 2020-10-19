80fb1632235ca390581e1254ee1bb67524d3a972
Alexander105 <sadorsadoff@gmail.com>
2020-10-19 22:21:04 +0300

    ClassWhereTaskRecommit

--- core/DB/Update.php
+++ core/DB/Update.php
@@ -6,35 +6,19 @@ namespace Core\DB;
 
 class Update
 {
-//    private $connector;
     private $tableName = '';
     private $columns ='';
     private $values;
-//    private $id;
-
-//    public function __construct()
-//    {
-//        $connectObj = new Connector();
-//        $this->connector = $connectObj->connectDB();
-//    }
-
-//    public function setId($id){
-//        if (!empty($id)){
-//                $this->id = $id;
-//        }
-//    }
 
     public function setConditions(array $condition = []){
         if(!empty($condition)){
             foreach ($condition as $columnsName => $value){
                 if (empty($this->columns)){
                     $this->values = $columnsName . '= \'' . $value . '\'';
-//                    $this->columns = $columnsName;
-//                    $this->values = '\'' . $value . '\'';
+
                 } else{
                     $this->values = ',' . $columnsName . '= \'' . $value . '\'';
-//                    $this->columns = ',' . $columnsName;
-//                    $this->values = ',' . '\'' . $value . '\'';
+
                 }
             }
         }
@@ -43,10 +27,7 @@ class Update
     public function  setTableName(string $name){
         $this->tableName = $name;
     }
-//    public function updateData(){
-//       var_export($this->updateString());
-//        return mysqli_query($this->connector, $this->updateString());
-//    }
+
 
     public function updateString()
     {
